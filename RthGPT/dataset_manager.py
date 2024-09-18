8e8583ab9cad8caf87a76753859eeed5228ee52b
rohethan <rohethan.devstuff@entasia.fr>
2024-09-18 15:26:52 +0200

    working training code, training test todo

--- RthGPT/dataset_manager.py
+++ RthGPT/dataset_manager.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import numpy as np
 
 class HuggingfaceWikipediaFr():
     def __init__(self):
@@ -12,24 +13,27 @@ class HuggingfaceWikipediaFr():
 
     def tokenized_data_generator(self):
         from RthGPT.tokenizer import RthTokenizer
-        tokenizer = RthTokenizer("../wiki16k.json")
+        tokenizer = RthTokenizer("wiki16k.json")
         for element in self.dst:
             tokenized_text = tokenizer.tokenizer.encode(element["text"])
             tokenized_text = tokenized_text.ids
             #print(tokenized_text)
 
+            i = 0
             increasing_content = []
             for token in tokenized_text:
                 ic_to_yield = increasing_content[:2048] + [0]*(2048-len(increasing_content))
-                yield (ic_to_yield, token)
+
+                yield (np.array(ic_to_yield).reshape(1, len(ic_to_yield)), np.array(i).reshape(1,1)), np.array(token).reshape(1,1)
                 increasing_content.append(token)
+                i += 1
 
 
 
 
     def tensorflow_dataset_generator(self):
-        output_type = (tf.int32, tf.int32)
-        output_shape = (tf.TensorShape([None, 2048]), tf.TensorShape([None, 1]))
+        output_type = (tf.int32, tf.int32), tf.int32
+        output_shape = (tf.TensorShape([None, 2048]), tf.TensorShape([None, 1])), tf.TensorShape([None, 1])
         tfds = tf.data.Dataset.from_generator(self.tokenized_data_generator, output_types=output_type, output_shapes=output_shape)
         return tfds
 
--- test_train.py
+++ test_train.py
@@ -5,14 +5,14 @@ model = RthGPT.build_model(context_length=2048,
                            embedding_size=128,
                            vocab_size=16384,
                            n_attention_blocks=16,
-                           attention_heads=4,
-                           after_attention_dense_ratio=4)
+                           attention_heads=2,
+                           after_attention_dense_ratio=2)
 model.summary()
 
 dataset = RthGPT.HuggingfaceWikipediaFr()
 
 print("model compiling start")
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 print("model compiled")
 print("model fit start")
 generator = dataset.tensorflow_dataset_generator()
